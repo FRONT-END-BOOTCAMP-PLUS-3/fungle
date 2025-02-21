2f6574e30c9d2c212b59def40c36d4b2b0140196
m01310g <m01310g@gmail.com>
2025-02-21 16:26:10 +0900

    [design] 02/21 초기화면 ui 구현 #60

---
+++ app/(anon)/page.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5rem;
+  height: 100vh;
+`;
+
+export const Logo = styled.img`
+  width: 8rem;
+`;
