3a82dbe712ce9940418c89ebcdc0d59e1ded187c
m01310g <m01310g@gmail.com>
2025-02-21 16:46:49 +0900

    [feat] 02/21 init page 버튼에 login으로 라우팅 추가 #60

--- app/(anon)/page.tsx
+++ app/(anon)/page.tsx
@@ -2,13 +2,20 @@
 
 import Button from "@/components/button/Button";
 import * as MC from "./page.styled";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+
+  const handleStart = () => {
+    router.push("/login");
+  };
+
   return (
     <MC.Container>
       <MC.Logo src="/logo/FUNGLE.svg" alt="logo" />
       <img src="/logo/Landing Logo.svg" alt="" />
-      <Button buttonSize="medium" fontSize="medium">
+      <Button buttonSize="medium" fontSize="medium" onClick={handleStart}>
         시작하기
       </Button>
     </MC.Container>
