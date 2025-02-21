f3c71df10981e1906147a1e63f1fda40458d65fa
m01310g <m01310g@gmail.com>
2025-02-21 11:25:48 +0900

    [design] 02/21 px rem으로 변경 #11

--- components/input/Input.styled.tsx
+++ components/input/Input.styled.tsx
@@ -16,15 +16,15 @@ export const InputComponent = styled.input<{ src?: string }>`
   border-color: var(--gray-300);
   border-radius: 0.625rem;
   font-size: var(--font-size-placeholder);
-  padding: 10px 16px;
+  padding: 0.625rem 1rem;
   text-align: left;
   ${({ src }) =>
     src &&
     `
     background-image: url(${src});
-    background-size: 20px 20px;
-    background-position: 14px center;
+    background-size: 1.25rem 1.25rem;
+    background-position: 0.875rem center;
     background-repeat: no-repeat;
-    padding-left: 48px;
+    padding-left: 3rem;
   `}
 `;
