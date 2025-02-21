fd7a06db401b3ad585828a1feb3dc611ef5ba6c9
binwoonki <vinwoonki98@gmail.com>
2025-02-21 14:14:54 +0900

    [chore] 02/21 filter option 상수 설정

---
+++ constants/COMMUNITY_POST_LIST.ts
@@ -0,0 +1,11 @@
+export const COMMUNITY_POST = [
+  { value: "title", label: "제목" },
+  { value: "author", label: "작성자" },
+  { value: "content", label: "내용" },
+];
+
+export const COMMUNITY_POST_LIST = [
+  { value: "latest", label: "최신순" },
+  { value: "likes", label: "좋아요순" },
+  { value: "comments", label: "댓글많은순" },
+];
