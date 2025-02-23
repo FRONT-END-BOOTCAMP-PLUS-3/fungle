23c9d15b8cf48f6fda8fcf6b3351f6f0bc1288af
binwoonki <vinwoonki98@gmail.com>
2025-02-23 19:04:17 +0900

    [design] 02/23 커뮤니티 게시글 리스트 필터링 UI

---
+++ app/user/community/components/CommunityListFilter.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ListFilterWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const EditButton = styled.button`
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+`;
