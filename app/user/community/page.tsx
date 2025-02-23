1e26970e6dc1b9832460008520833105c4ebb849
binwoonki <vinwoonki98@gmail.com>
2025-02-23 19:03:07 +0900

    [design] 02/23 커뮤니티 게시글 리스트 페이지 UI

--- app/user/community/page.tsx
+++ app/user/community/page.tsx
@@ -1,2 +1,27 @@
-// commit test
-// commit test
+"use client";
+import { useState } from "react";
+import { Main } from "./CommunityList.styled";
+
+import CommunitySearchFilter from "./components/CommunitySearchFilter";
+import CommunityListFilter from "./components/CommunityListFilter";
+import CommunityPostList from "./components/CommunityPostList";
+import CommunityPagination from "./components/CommunityPagination";
+
+const Page = () => {
+  const [selectedCommunity, setSelectedCommunity] = useState("all");
+
+  return (
+    <Main>
+      <CommunitySearchFilter
+        selectedCommunity={selectedCommunity}
+        setSelectedCommunity={setSelectedCommunity}
+      />
+      <CommunityListFilter />
+      <CommunityPostList selectedCommunity={selectedCommunity} />
+
+      <CommunityPagination />
+    </Main>
+  );
+};
+
+export default Page;
