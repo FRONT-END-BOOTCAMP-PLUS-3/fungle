de7fefb042be2969e60abb06608ba28c57cc088e
m01310g <m01310g@gmail.com>
2025-02-24 22:35:59 +0900

    [feat] 02/24 db에서 email로 user 정보 가져오는 함수 구현 #95

--- infrastructure/repositories/PrAuthRepository.ts
+++ infrastructure/repositories/PrAuthRepository.ts
@@ -1,1 +1,28 @@
-// commit test
+import { AuthRepository } from "@/domain/repositories/AuthRepository";
+import { prisma } from "../config/prisma";
+import { Prisma } from "@prisma/client";
+
+export class PrAuthRepository implements AuthRepository {
+  async findByEmail(email: string): Promise<Prisma.userGetPayload<{
+    select: {
+      id: true;
+      nickname: true;
+      userEmail: true;
+      password: true;
+      createdAt: true;
+      type: true;
+    };
+  }> | null> {
+    return await prisma.user.findUnique({
+      where: { userEmail: email },
+      select: {
+        id: true,
+        nickname: true,
+        userEmail: true,
+        password: true,
+        createdAt: true,
+        type: true,
+      },
+    });
+  }
+}
