6502879d3cfb3427d51215c7475a3cf4bc2a0f0f
binwoonki <vinwoonki98@gmail.com>
2025-02-25 14:13:07 +0900

    [design] 02/25 커뮤니티 수정 페이지 스타일 파일 #116

---
+++ app/user/community/[id]/edit/CommunityEditPage.styled.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Main = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  overflow-y: auto;
+  padding: 0 1.25rem 1.25rem 1.25rem;
+  margin-top: 1.25rem;
+`;
