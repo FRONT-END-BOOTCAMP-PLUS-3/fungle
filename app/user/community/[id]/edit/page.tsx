37e321c63a7889704660a7ac3ad6f9b4da7310dd
binwoonki <vinwoonki98@gmail.com>
2025-02-25 14:44:48 +0900

    [refactor] 02/25  커뮤니티 Form 분리로 인한 코드 수정

--- app/user/community/[id]/edit/page.tsx
+++ app/user/community/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-import CommunityPostEditForm from "./components/CommunityPostEditForm";
+import CommunityPostForm from "../../components/CommunityPostForm";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const postId = (await params).id;
@@ -6,7 +6,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   console.log(postId);
   // postId로 해당 게시글 가져오기
 
-  return <CommunityPostEditForm />;
+  return <CommunityPostForm />;
 };
 
 export default Page;
