35105cab3b2570dbe6a935e857037fbd0abdcfaf
binwoonki <vinwoonki98@gmail.com>
2025-02-25 14:09:45 +0900

    [design] 02/25 커뮤니티 수정 페이지 UI

--- app/user/community/[id]/edit/page.tsx
+++ app/user/community/[id]/edit/page.tsx
@@ -1,1 +1,12 @@
-// commit test
+import CommunityPostEditForm from "./components/CommunityPostEditForm";
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const postId = (await params).id;
+
+  console.log(postId);
+  // postId로 해당 게시글 가져오기
+
+  return <CommunityPostEditForm />;
+};
+
+export default Page;
