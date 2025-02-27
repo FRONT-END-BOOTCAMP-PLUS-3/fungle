1b32b7c9fca2868f409d2c95741d74511af86efa
binwoonki <vinwoonki98@gmail.com>
2025-02-27 16:10:50 +0900

    [feat] 02/27 커뮤니티 게시글 stats 컴포넌트 분리 및 DB 연동  #34

---
+++ app/user/community/components/CommunityPostStats.styled.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styled from "styled-components";
+
+export const PostStatsItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.3125rem;
+  font-size: 0.75rem;
+  align-items: center;
+`;
