46c41fc9f55543116cdc01d9de22f75a2dbee504
Hyein <snhyein2001@gmail.com>
2025-02-27 16:21:57 +0900

    [design] 02/27 책 생성 UI #158

--- components/dropdown/Dropdown.tsx
+++ components/dropdown/Dropdown.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 import {
   DropdownWrapper,
@@ -25,46 +26,61 @@ const Dropdown = ({
   selected,
   size = "default",
 }: DropdownProps) => {
-  const selectedLabel = Array.isArray(selected)
-    ? selected
-        .map(
-          (value) =>
-            options.find((option) => option.value === value)?.label || ""
-        )
-        .filter((value) => value)
-        .join(", ") || "선택하세요"
-    : options.find((option) => option.label === selected)?.label ||
-      "선택하세요";
+  const menuRef = useRef<HTMLDivElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen && menuRef.current) {
+      setTimeout(() => {
+        menuRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
+      }, 100);
+    }
+  }, [isOpen]); 
 
   return (
     <DropdownWrapper size={size}>
       <Menu>
-        {({ open }) => (
-          <div>
-            <MenuButton as={StyledButton} size={size}>
-              {selectedLabel}
-              <DropdownArrow
-                src="/icon/dropdown_arrow.svg"
-                alt="dropdown arrow"
-              />
-            </MenuButton>
-            {open && (
-              <MenuItems as={StyledMenu}>
-                {options.map((option, index) => (
-                  <MenuItem key={option.value} as="div">
-                    <StyledItem
-                      onClick={() => onSelect(option.value)}
-                      $isFirst={index === 0}
-                      $isLast={index === options.length - 1}
-                    >
-                      {option.label}
-                    </StyledItem>
-                  </MenuItem>
-                ))}
-              </MenuItems>
-            )}
-          </div>
-        )}
+        {({ open }) => {
+          useEffect(() => {
+            setIsOpen(open);
+          }, [open]);
+
+          const selectedLabel = Array.isArray(selected)
+            ? selected
+                .map(
+                  (value) =>
+                    options.find((option) => option.value === value)?.label || ""
+                )
+                .filter((value) => value)
+                .join(", ") || "선택하세요"
+            : options.find((option) => option.value === selected)?.label || "선택하세요";
+
+          return (
+            <div>
+              <MenuButton as={StyledButton} size={size}>
+                {selectedLabel}
+                <DropdownArrow src="/icon/dropdown_arrow.svg" alt="dropdown arrow" />
+              </MenuButton>
+              {open && (
+                <MenuItems as={StyledMenu} ref={menuRef}>
+                  {options.map((option, index) => (
+                    <MenuItem key={option.value} as="div">
+                      <StyledItem
+                        onClick={() => {
+                          onSelect(option.value);
+                        }}
+                        $isFirst={index === 0}
+                        $isLast={index === options.length - 1}
+                      >
+                        {option.label}
+                      </StyledItem>
+                    </MenuItem>
+                  ))}
+                </MenuItems>
+              )}
+            </div>
+          );
+        }}
       </Menu>
     </DropdownWrapper>
   );
