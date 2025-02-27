3102bc82563e8d82f9484cefdd55d4f5668a1739
binwoonki <vinwoonki98@gmail.com>
2025-02-27 16:12:44 +0900

    [feat] 02/27 커뮤니티 stats 함수 분리 #34

--- app/user/community/utils/getPostStats.ts
+++ app/user/community/utils/getPostStats.ts
@@ -1,32 +1,24 @@
-interface PostStats {
-  id: number;
-  title: string;
-  status: string;
-  genre: string;
-  author: string;
-  content: string;
-  time: string;
-  likes: number;
-  views: number;
-  commentCount: number;
-}
-export const getPostStats = (post: PostStats) => [
+import { PostWithCountAndRecruitmentDto } from "@/application/usecases/community/dto/PostWithCountAndRecruitmentDto";
+
+export const getPostStats = (
+  post: PostWithCountAndRecruitmentDto
+): { id: string; icon: string; alt: string; count: number }[] => [
   {
     id: "likes",
     icon: "/icon/heart.svg",
     alt: "좋아요 아이콘",
-    count: post.likes,
+    count: post.communityPostLikeCount,
   },
   {
     id: "views",
     icon: "/icon/eye.svg",
     alt: "조회수 아이콘",
-    count: post.views,
+    count: post.view,
   },
   {
     id: "comments",
     icon: "/icon/talk.svg",
     alt: "댓글 아이콘",
-    count: post.commentCount,
+    count: post.communityCommentCount,
   },
 ];
