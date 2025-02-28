1997b77ad2c05f9bffc92a18f070efffd70d2d79
m01310g <m01310g@gmail.com>
2025-02-28 14:50:33 +0900

    [design] 02/28 내가 쓴 글/소설, 펀딩 금액 조회하는 컴포넌트 구현 #168

---
+++ app/user/components/ProfileInfo.tsx
@@ -0,0 +1,23 @@
+import { InfoBox, InfoSection } from "./ProfileInfo.sylted";
+
+const ProfileInfo = () => {
+  return (
+    <InfoSection>
+      {/* 백엔드 연결 후 각 count로 변경 */}
+      <InfoBox>
+        <h5>내가 쓴 글</h5>
+        <p>12</p>
+      </InfoBox>
+      <InfoBox>
+        <h5>내가 쓴 소설</h5>
+        <p>2</p>
+      </InfoBox>
+      <InfoBox>
+        <h5>펀딩 금액</h5>
+        <p>1,000,000</p>
+      </InfoBox>
+    </InfoSection>
+  );
+};
+
+export default ProfileInfo;
