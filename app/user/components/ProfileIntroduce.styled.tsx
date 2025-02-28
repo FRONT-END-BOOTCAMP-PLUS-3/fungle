c003dbb61e02527e8d950cf4a2ab944e97ae784a
m01310g <m01310g@gmail.com>
2025-02-28 14:52:29 +0900

    [design] 02/28 작가 소개 컴포넌트 구현 #168

---
+++ app/user/components/ProfileIntroduce.styled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const IntroduceHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const IntroduceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
+  p {
+    text-align: center;
+  }
+`;
