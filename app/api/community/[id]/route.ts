c18bb4a20623bf30bf29adb04af84caf98ba4e66
binwoonki <vinwoonki98@gmail.com>
2025-02-28 13:29:19 +0900

    [feat] 02/28 커뮤니티 상세페이지 게시글 GET API 구현

--- app/api/community/[id]/route.ts
+++ app/api/community/[id]/route.ts
@@ -1,1 +1,39 @@
-//commit
+import { DfPostDetailUsecase } from "@/application/usecases/community/DfPostDetailUsecase";
+
+import { PrCommunityPostRepository } from "@/infrastructure/repositories/PrCommunityPostRepository";
+import { PrUserRepository } from "@/infrastructure/repositories/PrUserRepository";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  requets: NextRequest,
+  { params }: { params: { id: number } }
+) => {
+  const { id } = params;
+
+  if (!id) return "ID가 없습니다.";
+
+  try {
+    const communityPostRepository = new PrCommunityPostRepository();
+    const userRepository = new PrUserRepository();
+
+    const postDetailUsecase = new DfPostDetailUsecase(
+      communityPostRepository,
+      userRepository
+    );
+
+    const postDetail = await postDetailUsecase.execute(id);
+
+    return NextResponse.json(postDetail);
+  } catch (error: unknown) {
+    let message = "Unknown error";
+    let status = 500;
+    if (error instanceof Error) {
+      message = error.message;
+      const typedError = error as { status?: number };
+      if (typedError.status) {
+        status = typedError.status;
+      }
+    }
+    return NextResponse.json({ error: message }, { status });
+  }
+};
