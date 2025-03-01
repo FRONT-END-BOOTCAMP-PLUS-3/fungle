b69c5816dc01db3c09a801c55d738c96effcc60e
m01310g <m01310g@gmail.com>
2025-03-02 01:37:22 +0900

    [feat] 03/02 모달 버튼 함수 수정 #190

--- app/user/components/ProfileIntroduce.tsx
+++ app/user/components/ProfileIntroduce.tsx
@@ -16,15 +16,11 @@ const ProfileIntroduce = () => {
   const introduce =
     user?.introduce || "소개글이 비어있습니다.\n나를 나타내는 글을 적어주세요.";
 
-  const handleClick = () => {
-    openModal();
-  };
-
   return (
     <>
       <IntroduceHeader>
         <h5>소개</h5>
-        <Button buttonSize="small" onClick={handleClick}>
+        <Button buttonSize="small" onClick={openModal}>
           {user?.introduce ? "변경하기" : "작성하기"}
         </Button>
       </IntroduceHeader>
@@ -41,7 +37,9 @@ const ProfileIntroduce = () => {
               defaultValue={introduce || undefined}
             />
             <ButtonWrapper>
-              <Button backgroudColor="white">취소하기</Button>
+              <Button backgroudColor="white" onClick={onClose}>
+                취소하기
+              </Button>
               <Button>저장하기</Button>
             </ButtonWrapper>
           </ModalContentContainer>
