a8868adfd894e32416cadc2644970065efc0d0ea
binwoonki <vinwoonki98@gmail.com>
2025-03-01 21:53:07 +0900

    [feat] 03/01 커뮤니티 게시글 작성 기능 구현 및 작성 성공 시 상세페이지 이동 #27

--- app/user/community/components/CommunityPostForm.tsx
+++ app/user/community/components/CommunityPostForm.tsx
@@ -7,11 +7,15 @@ import { RECRUITMENT_FIELDS } from "@/constants/RECRUITMENT_FIELDS";
 
 import { useState } from "react";
 import { ButtonBox, TextAreaWrapper, Form } from "./CommunityPostForm.styled";
+import { useRouter } from "next/navigation";
 
-const CommunityPostForm = ({ defautlValue }: { defautlValue?: string }) => {
+const CommunityPostForm = ({ defaultValue }: { defaultValue?: string }) => {
   const [selectedFields, setSelectedFields] = useState<string[]>([]);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const router = useRouter();
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -36,40 +40,75 @@ const CommunityPostForm = ({ defautlValue }: { defautlValue?: string }) => {
     setContent(value);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("title:", title, "content", content);
+
+    if (!title || !content || selectedFields.length === 0) {
+      return alert("모든 값을 입력해주세요");
+    }
+    try {
+      const response = await fetch("/api/community/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          content,
+          fields: selectedFields,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("생성 실패");
+      }
+
+      const { postId } = await response.json();
+
+      alert("게시글이 생성되었습니다.상세 페이지로 이동합니다.");
+
+      router.push(`/user/community/${postId}`);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage("게시글 생성 에러가 발생했습니다.");
+      }
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <div>
-        <Input
-          label="제목"
-          hideLabel={true}
-          placeholder="제목을 입력해주세요"
-          onChange={handleTitleChange}
-        />
-      </div>
+    <>
+      {errorMessage && <div>{errorMessage}</div>}
+      <Form onSubmit={handleSubmit}>
+        <div>
+          <Input
+            label="제목"
+            hideLabel={true}
+            placeholder="제목을 입력해주세요"
+            onChange={handleTitleChange}
+          />
+        </div>
 
-      <Dropdown
-        options={RECRUITMENT_FIELDS}
-        onSelect={handleSelect}
-        selected={selectedFields}
-      />
-      <TextAreaWrapper>
-        <Textarea
-          ariaLabel="게시글 내용"
-          onChange={handleChangeContent}
-          height="100%"
-          defaultValue={defautlValue}
+        <Dropdown
+          options={RECRUITMENT_FIELDS}
+          onSelect={handleSelect}
+          selected={selectedFields}
         />
-        <ButtonBox>
-          <Button backgroudColor={"white"}>취소</Button>
-          <Button>작성</Button>
-        </ButtonBox>
-      </TextAreaWrapper>
-    </Form>
+        <TextAreaWrapper>
+          <Textarea
+            ariaLabel="게시글 내용"
+            onChange={handleChangeContent}
+            height="100%"
+            defaultValue={defaultValue}
+          />
+          <ButtonBox>
+            <Button backgroudColor={"white"}>취소</Button>
+            <Button>작성</Button>
+          </ButtonBox>
+        </TextAreaWrapper>
+      </Form>
+    </>
   );
 };
 
