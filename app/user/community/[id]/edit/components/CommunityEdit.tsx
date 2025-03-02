7ce9f85123cf2db1f043c741159b14b51486bfdc
binwoonki <vinwoonki98@gmail.com>
2025-03-02 20:15:10 +0900

    [feat] 03/02 커뮤니티 게시글 업데이트 기능 구현 #32

---
+++ app/user/community/[id]/edit/components/CommunityEdit.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import CommunityPostForm from "../../../components/CommunityPostForm";
+import { useRouter } from "next/navigation";
+
+type PostData = {
+  title: string;
+  content: string;
+  fields: string[];
+};
+const CommunityEdit = ({ postId }: { postId: string }) => {
+  const [postData, setPostData] = useState<PostData | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      try {
+        const response = await fetch(`/api/community/${postId}/edit`);
+
+        if (!response.ok) {
+          throw new Error("서버 오류");
+        }
+
+        const postData = await response.json();
+
+        setPostData({
+          title: postData.title,
+          content: postData.content,
+          fields: postData.recruitmentNames,
+        });
+      } catch {}
+    };
+
+    fetchPostData();
+  }, [postId]);
+
+  const initialSelectedFields = useMemo(() => {
+    return postData?.fields || [];
+  }, [postData?.fields]);
+
+  const handleEdit = async (
+    title: string,
+    content: string,
+    fields: string[]
+  ) => {
+    try {
+      const response = await fetch(`/api/community/${postId}/update`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          content,
+          fields,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("게시글 수정 실패");
+      }
+
+      const { postId: updatedPostId } = await response.json();
+
+      alert("게시글이 수정되었습니다.상세 페이지로 이동합니다.");
+
+      router.push(`/user/community/${updatedPostId}`);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(`게시글 수정 실패: ${error.message}`);
+      } else {
+        alert("게시글 수정 중 알 수 없는 오류가 발생했습니다.");
+      }
+    }
+  };
+
+  return (
+    <CommunityPostForm
+      mode="edit"
+      initalTitle={postData?.title}
+      initalContent={postData?.content}
+      initalSelectedFields={initialSelectedFields}
+      onSubmit={handleEdit}
+    />
+  );
+};
+
+export default CommunityEdit;
