c72a2d49a873b31fa7cdd232b5e339815f70202b
binwoonki <vinwoonki98@gmail.com>
2025-03-02 20:04:22 +0900

    [feat] 03/02 커뮤니티 업데이트 API 구현 #32

---
+++ app/api/community/[id]/update/route.ts
@@ -0,0 +1,55 @@
+import { DfVerifyRefreshToken } from "@/application/usecases/auth/DfVerifyRefreshToken";
+import { DfPostUpdateUsecase } from "@/application/usecases/community/DfPostUpdateUsecase";
+import { UserRepository } from "@/domain/repositories/UserRepository";
+import { PrCommunityPostRepository } from "@/infrastructure/repositories/PrCommunityPostRepository";
+import { PrUserRepository } from "@/infrastructure/repositories/PrUserRepository";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = await params;
+    const body = await request.json();
+
+    const cookieStore = await cookies();
+    const refreshToken = cookieStore.get("refreshToken")?.value;
+
+    if (!refreshToken) {
+      return NextResponse.json({ error: "No refresh token" }, { status: 401 });
+    }
+
+    const { title, content, fields } = body;
+
+    const userRepository: UserRepository = new PrUserRepository();
+    const verifyRefreshTokenUsecase = new DfVerifyRefreshToken(userRepository);
+    const verifiedUser = await verifyRefreshTokenUsecase.execute(refreshToken);
+
+    if (!verifiedUser) {
+      return NextResponse.json({ user: null }, { status: 401 });
+    }
+
+    const { userId } = verifiedUser;
+
+    const communityPostRepository = new PrCommunityPostRepository();
+    const postCreateUsecase = new DfPostUpdateUsecase(communityPostRepository);
+    const postId = await postCreateUsecase.execute(
+      userId,
+      id,
+      title,
+      content,
+      fields
+    );
+
+    return NextResponse.json(postId);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: error.message || "게시글 수정 실패" },
+        { status: 500 }
+      );
+    }
+  }
+};
