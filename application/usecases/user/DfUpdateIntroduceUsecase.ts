7aea5daa34756bd4607dc1304473d92573121ea2
m01310g <m01310g@gmail.com>
2025-03-03 18:04:25 +0900

    [feat] 03/03 작가 소개글 수정하는 usecase 구현 #199

---
+++ application/usecases/user/DfUpdateIntroduceUsecase.ts
@@ -0,0 +1,18 @@
+import { UserRepository } from "@/domain/repositories/UserRepository";
+
+export class DfUpdateIntroduceUsecase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(userId: string, introduce: string) {
+    const user = await this.userRepository.getUserById(userId);
+    if (!user) {
+      throw new Error("사용자를 찾을 수 없습니다.");
+    }
+
+    if (introduce.length > 200) {
+      throw new Error("소개글은 최대 200자까지 작성 가능합니다.");
+    }
+
+    await this.userRepository.updateIntroduce(userId, introduce);
+  }
+}
