1b30076b8c5eb3c45b1268f4ecf0f0827203408a
binwoonki <vinwoonki98@gmail.com>
2025-03-04 15:08:30 +0900

    [feat] 03/04 커뮤니티 게시글 좋아요 토글 기능 구현 infrastructure #36

--- infrastructure/repositories/PrCommunityPostLikeRepository.ts
+++ infrastructure/repositories/PrCommunityPostLikeRepository.ts
@@ -1,1 +1,42 @@
-// commit test
+import { CommunityPostLikeRepository } from "@/domain/repositories/CommunityPostLikeRepository";
+import { prisma } from "../config/prisma";
+
+export class PrCommunityPostLikeRepository
+  implements CommunityPostLikeRepository
+{
+  async toggleLike(id: string, userId: string): Promise<number> {
+    const postId = Number(id);
+    try {
+      const existingLike = await prisma.communityPostLike.findUnique({
+        where: {
+          userId_postId: { userId, postId },
+        },
+      });
+
+      if (existingLike) {
+        await prisma.communityPostLike.delete({
+          where: {
+            userId_postId: { userId, postId },
+          },
+        });
+      } else {
+        await prisma.communityPostLike.create({
+          data: {
+            userId,
+            postId,
+          },
+        });
+      }
+
+      const likeCount = await prisma.communityPostLike.count({
+        where: {
+          postId,
+        },
+      });
+
+      return likeCount;
+    } catch {
+      throw new Error("좋아요를 불러오는 중 오류가 발생했습니다.");
+    }
+  }
+}
