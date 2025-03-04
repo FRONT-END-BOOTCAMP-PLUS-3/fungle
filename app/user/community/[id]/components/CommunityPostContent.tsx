fe55e6a93af555d7588c6c7348effc0ae28ae68d
binwoonki <vinwoonki98@gmail.com>
2025-03-04 15:05:05 +0900

    [feat] 03/04 좋아요 버튼 클릭 시 API 호출하여 좋아요 토글 처리 구현 #36

--- app/user/community/[id]/components/CommunityPostContent.tsx
+++ app/user/community/[id]/components/CommunityPostContent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { CommunityPost } from "@prisma/client";
 import {
   CommunityPostContentSection,
@@ -5,6 +6,7 @@ import {
   CommunityLikeButton,
 } from "./CommunityPostContent.styled";
 import Image from "next/image";
+import { useState } from "react";
 
 type CommunityPostWithNicknameAndLikes = CommunityPost & {
   userNickname: string;
@@ -16,6 +18,31 @@ interface CommunityPostHeaderProps {
 }
 
 const CommunityPostContent = ({ postDetail }: CommunityPostHeaderProps) => {
+  const [likes, setLikes] = useState(postDetail.likes);
+  const handleLike = async () => {
+    try {
+      const response = await fetch("/api/community/like", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ postId: postDetail.id }),
+      });
+
+      if (!response.ok) {
+        throw new Error("서버 에러");
+      }
+
+      const { likeCount } = await response.json();
+
+      setLikes(likeCount);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(`좋아요 처리 중 오류 발생: ${error.message}`);
+      } else {
+        alert("좋아요 처리 중 알 수 없는 오류가 발생했습니다.");
+      }
+    }
+  };
+
   if (!postDetail) {
     return <main>게시글을 찾을 수 없습니다.</main>;
   }
@@ -23,14 +50,14 @@ const CommunityPostContent = ({ postDetail }: CommunityPostHeaderProps) => {
     <CommunityPostContentSection>
       <div style={{ whiteSpace: "pre-wrap" }}>{postDetail.content}</div>
       <CommunityPostLikeButtonBox>
-        <CommunityLikeButton>
+        <CommunityLikeButton onClick={handleLike}>
           <Image
             src="/icon/heart.svg"
             alt="좋아요 버튼"
             width={20}
             height={20}
           />
-          {postDetail.likes}
+          {likes}
         </CommunityLikeButton>
       </CommunityPostLikeButtonBox>
     </CommunityPostContentSection>
