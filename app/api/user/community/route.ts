ee83eaf62e0f31ce3ab57b4c0a7f6d8fbec4dbdd
m01310g <m01310g@gmail.com>
2025-03-05 16:31:11 +0900

    [feat] 03/05 모집 상태 변경 api 구현 #221

--- app/api/user/community/route.ts
+++ app/api/user/community/route.ts
@@ -1,5 +1,7 @@
 import { DfVerifyRefreshToken } from "@/application/usecases/auth/DfVerifyRefreshToken";
 import { DfPostDetailByUserIdUsecase } from "@/application/usecases/community/DfPostDetailByUserIdUsecase";
+import { DfPostStatusUpdateUsecase } from "@/application/usecases/community/DfPostStatusUpdateUsecase";
+import { CommunityPostRepository } from "@/domain/repositories/CommunityPostRepository";
 import { UserRepository } from "@/domain/repositories/UserRepository";
 import { PrCommunityPostRepository } from "@/infrastructure/repositories/PrCommunityPostRepository";
 import { PrUserRepository } from "@/infrastructure/repositories/PrUserRepository";
@@ -40,5 +42,46 @@ export const GET = async (req: NextRequest) => {
       { message: "서버에서 오류가 발생했습니다." },
       { status: 500 }
     );
+  }
+};
+
+export const PATCH = async (req: NextRequest) => {
+  try {
+    // 로그인 된 사용자인지 검증
+    const refreshToken = req.cookies.get("refreshToken")?.value;
+    if (!refreshToken) {
+      return NextResponse.json({ user: null }, { status: 401 });
+    }
+
+    const userRepository: UserRepository = new PrUserRepository();
+    const verifyRefreshTokenUsecase = new DfVerifyRefreshToken(userRepository);
+    const verifiedUser = await verifyRefreshTokenUsecase.execute(refreshToken);
+
+    const userId = verifiedUser?.id;
+
+    if (!userId) {
+      return NextResponse.json({ posts: [] }, { status: 400 });
+    }
+
+    const { postId } = await req.json();
+
+    const communityPostRepository: CommunityPostRepository =
+      new PrCommunityPostRepository();
+    const postStatusUpdateUsecase = new DfPostStatusUpdateUsecase(
+      communityPostRepository
+    );
+
+    await postStatusUpdateUsecase.execute(userId, postId);
+    return NextResponse.json(
+      { message: "모집 상태가 성공적으로 변경되었습니다!" },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: error.message || "모집 상태 변경 실패" },
+        { status: 500 }
+      );
+    }
   }
 };
