0956a9b6c7348df7dc107471d73c7c278adbf5fd
binwoonki <vinwoonki98@gmail.com>
2025-03-05 14:27:22 +0900

    [feat] 03/05 댓글 등록 시 댓글 목록 자동 새로고침 기능 적용 #26

---
+++ components/comment/CommentCreateTextarea.tsx
@@ -0,0 +1,100 @@
+"use client";
+import Textarea from "../textarea/Textarea";
+import Button from "../button/Button";
+
+import { useState } from "react";
+import { ButtonWrapper } from "./CommentCreateTextarea.styled";
+
+type CommentCreateTextareaProps = {
+  isOpenTextarea: boolean;
+  setIsOpenTextarea: React.Dispatch<React.SetStateAction<boolean>>;
+  postId: string;
+  setTrigger: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const CommentCreateTextarea = ({
+  isOpenTextarea,
+  setIsOpenTextarea,
+  postId,
+  setTrigger,
+}: CommentCreateTextareaProps) => {
+  const [comment, setComment] = useState("");
+
+  const fetchCreateComment = async () => {
+    try {
+      const response = await fetch(`/api/community/comment/${postId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          comment,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("댓글 생성 실패");
+      }
+
+      const result = await response.json();
+
+      if (result) {
+        alert("댓글이 등록되었습니다.");
+        setIsOpenTextarea((prev) => !prev);
+        setComment("");
+      }
+
+      setTrigger((prev) => !prev);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(`댓글 생성 중 오류가 발생했습니다: ${error.message}`);
+      } else {
+        alert("댓글 생성 중 알 수 없는 오류가 발생했습니다.");
+      }
+    }
+  };
+
+  const handleCreateComment = async () => {
+    if (!comment) {
+      alert("댓글을 입력해주세요.");
+      return;
+    }
+
+    await fetchCreateComment();
+  };
+
+  return (
+    <>
+      {isOpenTextarea && (
+        <>
+          <Textarea
+            ariaLabel="댓글 입력"
+            placeholder="댓글을 입력해주세요."
+            height="20vh"
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <ButtonWrapper>
+            <Button
+              backgroudColor="white"
+              type="button"
+              buttonSize="small"
+              onClick={() => setIsOpenTextarea((prev) => !prev)}
+            >
+              취소
+            </Button>
+            <Button
+              backgroudColor="primary"
+              type="button"
+              buttonSize="small"
+              onClick={handleCreateComment}
+            >
+              등록
+            </Button>
+          </ButtonWrapper>
+        </>
+      )}
+    </>
+  );
+};
+
+export default CommentCreateTextarea;
