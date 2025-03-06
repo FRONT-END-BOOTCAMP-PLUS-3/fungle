d49341f1ff3909c7b824ec91594e0cff5bd3c2bc
binwoonki <vinwoonki98@gmail.com>
2025-03-06 17:01:57 +0900

    [refactor] 03/06 커뮤니티 댓글 조회 좋아요 누른 userId boolean 추가 #31

--- app/api/community/[id]/comments/route.ts
+++ app/api/community/[id]/comments/route.ts
@@ -1,5 +1,8 @@
+import { DfVerifyRefreshToken } from "@/application/usecases/auth/DfVerifyRefreshToken";
 import { DfPostDetailCommentUsecase } from "@/application/usecases/community/DfPostDetailCommentUsecase";
 import { PrCommunityCommentRepository } from "@/infrastructure/repositories/PrCommunityCommentRepository";
+import { PrUserRepository } from "@/infrastructure/repositories/PrUserRepository";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
@@ -14,7 +17,24 @@ export const GET = async (
       commentPostRepository
     );
 
-    const comments = await postCommentUsecase.execute(id);
+    const cookieStore = await cookies();
+    const refreshToken = cookieStore.get("refreshToken")?.value;
+
+    if (!refreshToken) {
+      return NextResponse.json({ error: "No refresh token" }, { status: 401 });
+    }
+
+    const userRepository = new PrUserRepository();
+    const verifyRefreshTokenUsecase = new DfVerifyRefreshToken(userRepository);
+    const verifiedUser = await verifyRefreshTokenUsecase.execute(refreshToken);
+
+    if (!verifiedUser) {
+      return NextResponse.json({ user: null }, { status: 401 });
+    }
+
+    const { id: userId } = verifiedUser;
+
+    const comments = await postCommentUsecase.execute(id, userId);
 
     return NextResponse.json(comments);
   } catch (error: unknown) {
