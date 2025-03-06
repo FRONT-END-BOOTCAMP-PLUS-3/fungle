f3bb53f08a1dadf33cc27248311b32639ef45d33
m01310g <m01310g@gmail.com>
2025-03-07 01:17:44 +0900

    [feat] 03/07 사용자 검증 api 수정 #268

--- infrastructure/config/userDi.ts
+++ infrastructure/config/userDi.ts
@@ -1,12 +1,12 @@
-import { DfVerifyAccessToken } from "@/application/usecases/auth/DfVerifyAccessToken";
+import { DfVerifyAccessTokenUsecase } from "@/application/usecases/auth/DfVerifyAccessTokenUsecase";
 import { PrUserRepository } from "../repositories/PrUserRepository";
 import { DfGetUserIdUsecase } from "@/application/usecases/auth/DfGetUserIdUsecase";
 
 export const userDi = {
   userRepository: new PrUserRepository(),
-  verifyTokenUsecase: new DfVerifyAccessToken(new PrUserRepository()),
+  verifyTokenUsecase: new DfVerifyAccessTokenUsecase(new PrUserRepository()),
   getUserIdUsecase: new DfGetUserIdUsecase(
     new PrUserRepository(),
-    new DfVerifyAccessToken(new PrUserRepository())
+    new DfVerifyAccessTokenUsecase(new PrUserRepository())
   ),
 };
