38f4f406425528b5b6d82dd8e497732796e45298
m01310g <m01310g@gmail.com>
2025-03-07 01:08:59 +0900

    [feat] 03/07 access token 검증하는 usecase 반환값에 decoded된 access token 정보 추가 #268

---
+++ application/usecases/auth/DfVerifyAccessTokenUsecase.ts
@@ -0,0 +1,34 @@
+import { UserRepository } from "@/domain/repositories/UserRepository";
+import { verifyAccessToken } from "@/utils/auth/jwt";
+import { User } from "@prisma/client";
+import { DecodedAccessToken } from "./dto/DecodedAccessToken";
+
+export class DfVerifyAccessTokenUsecase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(token: string): Promise<{
+    verifiedUser: Omit<
+      User,
+      "userEmail" | "password" | "createdAt" | "type"
+    > | null;
+    decodedAccessToken: DecodedAccessToken | null;
+  } | null> {
+    try {
+      // 토큰이 유효한지 검증
+      const decoded = verifyAccessToken(token);
+      if (!decoded || !decoded.userId)
+        return { verifiedUser: null, decodedAccessToken: null };
+
+      // db에 사용자가 존재하는지 검증
+      const user = await this.userRepository.getUserById(decoded.userId);
+      if (!user) return { verifiedUser: null, decodedAccessToken: null };
+
+      return {
+        verifiedUser: user,
+        decodedAccessToken: { userId: decoded.userId, exp: decoded.exp },
+      };
+    } catch (error) {
+      return null;
+    }
+  }
+}
