3c219dee4713effca73bfca6356543cd11e722f1
m01310g <m01310g@gmail.com>
2025-03-07 17:04:56 +0900

    [feat] 03/07 좋아요 누른 커뮤니티 게시글 조회 repository 구현 #278

--- domain/repositories/CommunityPostLikeRepository.ts
+++ domain/repositories/CommunityPostLikeRepository.ts
@@ -1,6 +1,11 @@
+import { PostWithCountAndRecruitmentDto } from "@/application/usecases/community/dto/LikedCommuityPostDto";
+
 export interface CommunityPostLikeRepository {
   toggleLike(
     id: string,
     userId: string
   ): Promise<{ likeCount: number; isLiked: boolean }>;
+  getLikedCommunityPostsByUserId(
+    userId: string
+  ): Promise<PostWithCountAndRecruitmentDto[]>;
 }
--- infrastructure/repositories/PrCommunityPostLikeRepository.ts
+++ infrastructure/repositories/PrCommunityPostLikeRepository.ts
@@ -1,5 +1,6 @@
 import { CommunityPostLikeRepository } from "@/domain/repositories/CommunityPostLikeRepository";
 import { prisma } from "../config/prisma";
+import { PostWithCountAndRecruitmentDto } from "@/application/usecases/community/dto/PostWithCountAndRecruitmentDto";
 
 export class PrCommunityPostLikeRepository
   implements CommunityPostLikeRepository
@@ -43,5 +44,65 @@ export class PrCommunityPostLikeRepository
     } catch {
       throw new Error("좋아요를 불러오는 중 오류가 발생했습니다.");
     }
+  }
+
+  async getLikedCommunityPostsByUserId(
+    userId: string
+  ): Promise<PostWithCountAndRecruitmentDto[]> {
+    const likedPosts = await prisma.communityPostLike.findMany({
+      where: { userId },
+      select: {
+        communityPosts: {
+          select: {
+            id: true,
+            title: true,
+            content: true,
+            createdAt: true,
+            status: true,
+            view: true,
+            userId: true,
+            _count: {
+              select: { communityPostLikes: true, communityComments: true },
+            },
+            PostRecruitments: {
+              select: {
+                RecruitmentCategory: {
+                  select: {
+                    name: true,
+                  },
+                },
+              },
+            },
+            user: {
+              select: {
+                nickname: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        communityPosts: {
+          createdAt: "desc",
+        },
+      },
+    });
+
+    return likedPosts.map((like) => ({
+      id: like.communityPosts.id,
+      title: like.communityPosts.title,
+      content: like.communityPosts.content,
+      createdAt: like.communityPosts.createdAt,
+      userNickname: like.communityPosts.user.nickname,
+      recruitmentNames: like.communityPosts.PostRecruitments.map(
+        (r) => r.RecruitmentCategory.name
+      ),
+      communityPostLikeCount: like.communityPosts._count.communityPostLikes,
+      communityCommentCount: like.communityPosts._count.communityComments,
+      view: like.communityPosts.view,
+      userId: like.communityPosts.userId,
+      status: like.communityPosts.status,
+      views: like.communityPosts.view,
+    }));
   }
 }
