cd82ce14cca529626d7e76b0dce178f2ce2171ad
m01310g <m01310g@gmail.com>
2025-03-07 17:06:31 +0900

    [feat] 03/07 좋아요 누른 목록 컴포넌트에 api 연결 #278

--- app/user/components/LikedPostList.tsx
+++ app/user/components/LikedPostList.tsx
@@ -12,45 +12,78 @@ import {
   PostTime,
   PostUserNickname,
 } from "../community/components/CommunityPostList.styled";
+import useAuthStore from "@/store/useAuthStore";
+import { useEffect, useState } from "react";
+import { realTimeView } from "../community/utils/realTimeView";
+import CommunityPostStats from "../community/components/CommunityPostStats";
+import { PostWithCountAndRecruitmentDto } from "@/application/usecases/community/dto/PostWithCountAndRecruitmentDto";
+import { RECRUITMENT_FIELDS } from "@/constants/RECRUITMENT_FIELDS";
 
 const LikedPostList = () => {
+  const { user } = useAuthStore();
+  const [posts, setPosts] = useState<PostWithCountAndRecruitmentDto[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("/api/user/community/liked", {
+          method: "GET",
+        });
+
+        if (!response.ok) throw new Error("게시글을 불러오는 데 실패했습니다.");
+
+        const data = await response.json();
+        setPosts(data.likedPosts);
+      } catch (error) {
+        setError("게시글을 불러오는 중 오류가 발생했습니다.");
+      }
+    };
+
+    fetchPosts();
+  }, [user]);
+
   return (
-    <PostAndLikedListWrapper>
-      {/* <PostAndLikedListWrapper key={post.id}> */}
-      {/* <Link href={`/user/community/${post.id}`}> */}
-      <Link href={`/user/community/1`}>
-        <PostBox>
-          <PostStatusWithNickname>
-            {/* <PostStatus status={post.status}>
-                {post.status === "recruiting" ? "모집중" : "모집완료"}
-              </PostStatus> */}
-            <PostStatus status="recruiting">
-              모집중
-              {/* {post.status === "recruiting" ? "모집중" : "모집완료"} */}
-            </PostStatus>
-          </PostStatusWithNickname>
-          {/* <p>{post.title}</p> */}
-          <p>제목</p>
-          {/* <PostContent>{post.content}</PostContent> */}
-          <PostContent>내용</PostContent>
-          {/* {post.recruitmentNames.length > 0 && (
-              <PostRecruitment>
-                {post.recruitmentNames.join(", ")}
-              </PostRecruitment>
-            )} */}
-          <PostRecruitment>모집 분야</PostRecruitment>
-          <PostFooter>
-            <PostInfo>
-              {/* <PostUserNickname>{post.userNickname}</PostUserNickname> */}
-              <PostUserNickname>한교동짬뽕</PostUserNickname>
-              {/* <PostTime>{realTimeView(new Date(post.createdAt))}</PostTime> */}
-              <PostTime>시간</PostTime>
-            </PostInfo>
-            <PostStats>{/* <CommunityPostStats post={post} /> */}</PostStats>
-          </PostFooter>
-        </PostBox>
-      </Link>
-    </PostAndLikedListWrapper>
+    <>
+      {posts.map((post) => (
+        <PostAndLikedListWrapper key={post.id}>
+          <Link href={`/user/community/${post.id}`}>
+            <PostBox>
+              <PostStatusWithNickname>
+                <PostStatus status={post.status}>
+                  {post.status === "recruiting" ? "모집중" : "모집완료"}
+                </PostStatus>
+              </PostStatusWithNickname>
+              <p>{post.title}</p>
+              <PostContent>{post.content}</PostContent>
+              {post.recruitmentNames.length > 0 && (
+                <PostRecruitment>
+                  {post.recruitmentNames
+                    .map((name) => {
+                      const field = RECRUITMENT_FIELDS.find(
+                        (f) => f.value === name
+                      );
+                      return field ? field.label : name;
+                    })
+                    .join(", ")}{" "}
+                </PostRecruitment>
+              )}
+              <PostFooter>
+                <PostInfo>
+                  <PostUserNickname>{post.userNickname}</PostUserNickname>
+                  <PostTime>{realTimeView(new Date(post.createdAt))}</PostTime>
+                </PostInfo>
+                <PostStats>
+                  <CommunityPostStats post={post} />
+                </PostStats>
+              </PostFooter>
+            </PostBox>
+          </Link>
+        </PostAndLikedListWrapper>
+      ))}
+    </>
   );
 };
 
