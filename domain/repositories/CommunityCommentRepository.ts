9f3a09fc8166c6f9b18335b5120249a113aa11b4
binwoonki <vinwoonki98@gmail.com>
2025-03-09 00:07:02 +0900

    [refactor] 03/09 CommunityCommentRepository create parentId 추가 #274

--- domain/repositories/CommunityCommentRepository.ts
+++ domain/repositories/CommunityCommentRepository.ts
@@ -23,7 +23,12 @@ export type CommentWithRelations = Prisma.CommunityCommentGetPayload<{
 export interface CommunityCommentRepository {
   findAll(postId: string, userId: string): Promise<CommentWithRelations[]>;
   commentCount(postId: string): Promise<number>;
-  create(id: string, userId: string, comment: string): Promise<boolean>;
+  create(
+    id: string,
+    userId: string,
+    comment: string,
+    parentId: string
+  ): Promise<boolean>;
   commentUpdate(id: string, userId: string, content: string): Promise<boolean>;
   commentDelete(id: string, userId: string): Promise<boolean>;
 }
