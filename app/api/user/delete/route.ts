db9fb649aa24e71853f39620ad6e6eabb086680c
m01310g <m01310g@gmail.com>
2025-03-09 23:08:53 +0900

    [feat] 03/09 진행 중인 펀딩이 있는 경우 회원 탈퇴 불가능하도록 변경 #296

--- app/api/user/delete/route.ts
+++ app/api/user/delete/route.ts
@@ -1,10 +1,13 @@
+import { DfFundingUsecase } from "@/application/usecases/funding/DfFundingUsecase";
 import { DfEpisodeByUserIdUsecase } from "@/application/usecases/novel/DfEpisodeByUserIdUsecase";
 import { DfNovelByUserIdUsecase } from "@/application/usecases/novel/DfNovelByUserIdUsecase";
 import { DfDeleteUserUsecase } from "@/application/usecases/user/DfDeleteUserUsecase";
+import { FundingRepository } from "@/domain/repositories/FundingRepository";
 import { NovelEpisodeRepository } from "@/domain/repositories/NovelEpisodeRepository";
 import { NovelRepository } from "@/domain/repositories/NovelRepository";
 import { UserRepository } from "@/domain/repositories/UserRepository";
 import { userDi } from "@/infrastructure/config/userDi";
+import { PrFundingRepository } from "@/infrastructure/repositories/PrFundingRepository";
 import { PrNovelEpisodeRepository } from "@/infrastructure/repositories/PrNovelEpisodeRepository";
 import { PrNovelRepository } from "@/infrastructure/repositories/PrNovelRepostiory";
 import { PrUserRepository } from "@/infrastructure/repositories/PrUserRepository";
@@ -22,11 +25,13 @@ export const DELETE = async (req: NextRequest) => {
     );
   }
 
-  const userRepository: UserRepository = new PrUserRepository();
   const fileService = new FileService();
+
+  const userRepository: UserRepository = new PrUserRepository();
   const novelRepository: NovelRepository = new PrNovelRepository();
   const novelEpisodeRepository: NovelEpisodeRepository =
     new PrNovelEpisodeRepository();
+  const fundingRepository: FundingRepository = new PrFundingRepository();
 
   const episodesByUserIdUsecase = new DfEpisodeByUserIdUsecase(
     novelEpisodeRepository
@@ -35,24 +40,31 @@ export const DELETE = async (req: NextRequest) => {
     novelRepository,
     episodesByUserIdUsecase
   );
+  const fundingUsecase = new DfFundingUsecase(fundingRepository);
   const deleteUserUsecase = new DfDeleteUserUsecase(
     userRepository,
     novelByUserIdUsecase,
-    fileService
+    fileService,
+    fundingUsecase
   );
 
-  await deleteUserUsecase.execute(userId);
+  const isSuccess = await deleteUserUsecase.execute(userId);
+
+  if (!isSuccess)
+    return NextResponse.json(
+      {
+        error: "진행 중인 펀딩이 있는 경우 회원 탈퇴가 불가능합니다.",
+      },
+      { status: 400 }
+    );
 
   const cookieStore = await cookies();
 
-  const response = NextResponse.json(
-    {
-      message: "회원 탈퇴가 완료되었습니다.",
-    },
-    { status: 200 }
-  );
   cookieStore.set("accessToken", "", { maxAge: 0, path: "/" });
   cookieStore.set("refreshToken", "", { maxAge: 0, path: "/" });
 
-  return response;
+  return NextResponse.json(
+    { message: "회원 탈퇴가 완료되었습니다." },
+    { status: 200 }
+  );
 };
