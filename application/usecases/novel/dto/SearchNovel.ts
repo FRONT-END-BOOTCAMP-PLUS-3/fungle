94c8c492f9b79d45a9724c7921e6b222ec6a12b3
Hyein <snhyein2001@gmail.com>
2025-03-09 14:17:00 +0900

    [fix] 03/09 DTO any 삭제 #286

--- application/usecases/novel/dto/SearchNovel.ts
+++ application/usecases/novel/dto/SearchNovel.ts
@@ -1,3 +1,5 @@
+import { Novel } from "@prisma/client"; 
+
 export class SearchNovelDTO {
   id: number;
   title: string;
@@ -6,10 +8,10 @@ export class SearchNovelDTO {
   genres: string[]; 
   fundingStatus: string; 
 
-  constructor(novel: any, author: string, genres: string[]) {
+  constructor(novel: Pick<Novel, "id" | "title" | "image">, author: string, genres: string[]) {
     this.id = novel.id;
     this.title = novel.title;
-    this.image = novel.image;
+    this.image = novel.image || "/image/book.svg"; 
     this.author = author;
     this.genres = genres;
     this.fundingStatus = "1단계 ⭐"; 
