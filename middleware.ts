c64862147e7fe7ae014cf2dac9c5f08cdeb1d5dc
m01310g <m01310g@gmail.com>
2025-03-09 15:45:26 +0900

    [feat] 03/09 로그인 상태 따른 리다이렉트 url 설정 #291

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const middleware = (req: NextRequest) => {
-  const publicRoutes = ["/login", "/signup", "/novel", "/community"];
-  const refreshToken = req.cookies.get("refreshToken")?.value;
+  const authRoutes: Record<string, string> = {
+    "/login": "/user/novel",
+    "/novel": "/user/novel",
+    "/community": "/user/community",
+  };
+  const publicRoutes = ["/signup"];
+
+  const accessToken = req.cookies.get("accessToken")?.value;
 
   // publicRoute에서는 다음 동작으로 넘어감
   if (publicRoutes.some((route) => req.nextUrl.pathname === route)) {
     return NextResponse.next();
   }
 
+  // 사용자가 로그인된 상태면 `/login` 페이지에서 `/user/novel`으로 리다이렉트
+  if (accessToken && authRoutes[req.nextUrl.pathname]) {
+    return NextResponse.redirect(
+      new URL(authRoutes[req.nextUrl.pathname], req.url)
+    );
+  }
+
   // 토큰이 없을 경우 로그인 페이지로 리다이렉트
-  if (!refreshToken) {
+  if (!accessToken) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
@@ -21,5 +34,5 @@ export default middleware;
 
 // 실행될 경로 설정
 export const config = {
-  matcher: ["/user/:path*"],
+  matcher: ["/login", "/novel", "/community", "/user/:path*"],
 };
