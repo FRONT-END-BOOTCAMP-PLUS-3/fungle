d6098f7ea5468a37b423ba0499ace6ac3832cd41
binwoonki <vinwoonki98@gmail.com>
2025-03-10 16:15:36 +0900

    [refactor] 03/10 커뮤니티 상세 페이지 클라이언트 컴포넌트로 변경 #326

--- app/(main)/user/community/[id]/page.tsx
+++ app/(main)/user/community/[id]/page.tsx
@@ -1,44 +1,60 @@
+"use client";
 import { Main } from "./CommunityDetailPage.styled";
 
 import CommunityPostHeader from "./components/CommunityPostHeader";
 import CommunityPostContent from "./components/CommunityPostContent";
 import CommentHeader from "@/components/comment/CommentHeader";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Loading from "./loading";
+import { CommunityPost } from "@prisma/client";
+export type CommunityDetailPostType = CommunityPost & {
+  userNickname: string;
+  likes: number;
+  likedUserId: string[];
+};
+const Page = () => {
+  const [post, setPost] = useState<CommunityDetailPostType | null>(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const params = useParams();
+  const postId = params.id;
 
-interface PageParams {
-  params: Promise<{ id: string }>;
-}
+  useEffect(() => {
+    const fetchDetailPage = async () => {
+      try {
+        const response = await fetch(`/api/community/${postId}`);
 
-const Page = async ({ params }: PageParams) => {
-  const postId = (await params).id;
+        if (!response.ok) {
+          throw new Error(`서버 연결 실패: ${response.status}`);
+        }
 
-  let post;
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/community/${postId}`,
-      {
-        cache: "no-store",
+        const post = await response.json();
+        setPost(post);
+      } catch (error: unknown) {
+        let message = "게시글을 찾는 중 알 수 없는 오류가 발생했습니다.";
+        if (error instanceof Error) {
+          message = error.message;
+        }
+        setErrorMessage(message);
       }
-    );
+    };
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch post: ${response.status}`);
-    }
+    fetchDetailPage();
+  }, [postId]);
 
-    post = await response.json();
-  } catch (error: unknown) {
-    let errorMessage = "게시글을 찾는 중 알 수 없는 오류가 발생했습니다.";
-    if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-    return <main>{errorMessage}</main>;
-  }
   return (
     <Main>
-      <CommunityPostHeader postDetail={post} />
-
-      <CommunityPostContent postDetail={post} />
-
-      <CommentHeader post={post} />
+      {errorMessage ? (
+        <main>{errorMessage}</main>
+      ) : !post ? (
+        <Loading />
+      ) : (
+        <>
+          <CommunityPostHeader postDetail={post} />
+          <CommunityPostContent postDetail={post} />
+          <CommentHeader post={post} />
+        </>
+      )}
     </Main>
   );
 };
