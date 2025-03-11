c6f6f2a0970983b8ecb0620bd34e00ad7fdd21a4
m01310g <m01310g@gmail.com>
2025-03-11 15:16:01 +0900

    [feat] 03/11 관리자 회원 관리 ui 구현 #337

--- app/admin/user/page.tsx
+++ app/admin/user/page.tsx
@@ -1,7 +1,27 @@
 "use client";
 
+import { AdminHeader, AdminMain, Table } from "../AdminPage.styled";
+
 const Page = () => {
-  return <div>admin</div>;
+  return (
+    <AdminMain>
+      <AdminHeader>
+        <h1>회원 관리</h1>
+      </AdminHeader>
+      <Table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>이메일</th>
+            <th>닉네임</th>
+            <th>회원 유형</th>
+            <th>소개</th>
+            <th>프로필 사진</th>
+          </tr>
+        </thead>
+      </Table>
+    </AdminMain>
+  );
 };
 
 export default Page;
