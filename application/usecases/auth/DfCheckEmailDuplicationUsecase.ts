f95d91ad810c47dc6be4c3568dc9aafac35f7190
hyun9758 <jj020305@likelion.org>
2025-03-11 15:02:00 +0900

    [fix] 03/11 readonly 추가

--- application/usecases/auth/DfCheckEmailDuplicationUsecase.ts
+++ application/usecases/auth/DfCheckEmailDuplicationUsecase.ts
@@ -1,7 +1,7 @@
 import { UserRepository } from "@/domain/repositories/UserRepository";
 
 export class DfCheckEmailDuplicationUsecase {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
   async execute(email: string): Promise<boolean> {
     const user = await this.userRepository.findByEmail(email);
     return !!user;
