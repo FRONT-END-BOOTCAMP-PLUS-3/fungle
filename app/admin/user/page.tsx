2dafd7fc46941ce2bb08115f6cef7781a9f6054c
m01310g <m01310g@gmail.com>
2025-03-11 23:22:21 +0900

    [feat] 03/11 table에 사용자 정보 렌더링 #337

--- app/admin/user/page.tsx
+++ app/admin/user/page.tsx
@@ -1,8 +1,36 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { AdminHeader, AdminMain, Table } from "../AdminPage.styled";
+import { FindAllUserDto } from "@/application/usecases/user/dto/FindAllUser";
+import Image from "next/image";
 
 const Page = () => {
+  const [users, setUsers] = useState<FindAllUserDto[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/admin/user");
+        const data = await response.json();
+
+        if (!response.ok) {
+          setError(data.error);
+        }
+        setUsers(data.users);
+      } catch (error: unknown) {
+        setError(
+          error instanceof Error
+            ? error.message
+            : "사용자 데이터를 가져오는 데 실패했습니다."
+        );
+      }
+    };
+
+    fetchData();
+  }, [setUsers]);
+
   return (
     <AdminMain>
       <AdminHeader>
@@ -19,6 +47,31 @@ const Page = () => {
             <th>프로필 사진</th>
           </tr>
         </thead>
+        <tbody>
+          {users.length > 0 ? (
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.userEmail}</td>
+                <td>{user.nickname}</td>
+                <td>{user.type}</td>
+                <td>{user.introduce || "소개가 등록되지 않았습니다."}</td>
+                <td>
+                  <Image
+                    src={user.profileImage || "/image/profile.svg"}
+                    alt={`${user.nickname} 프로필 사진`}
+                    width={100}
+                    height={100}
+                  />
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={6}>{error}</td>
+            </tr>
+          )}
+        </tbody>
       </Table>
     </AdminMain>
   );
