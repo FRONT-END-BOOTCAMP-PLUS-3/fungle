ad0f9704524b4fc9cde774f34b189f0d7dd124d1
m01310g <m01310g@gmail.com>
2025-03-12 16:34:03 +0900

    [fix] 03/12 사용자 검증 경로 수정 #375

--- components/authInitializer/AuthInitializer.tsx
+++ components/authInitializer/AuthInitializer.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import useAuthStore from "@/store/useAuthStore";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 const AuthInitializer = () => {
   const { refreshAuth } = useAuthStore();
+  const pathname = usePathname();
+
+  const excludedPaths = ["/", "/login", "/register"];
+  const shouldSkipAuthCheck = excludedPaths.includes(pathname);
 
   useEffect(() => {
-    refreshAuth();
-  }, [refreshAuth]);
+    if (!shouldSkipAuthCheck) {
+      refreshAuth();
+    }
+  }, [refreshAuth, shouldSkipAuthCheck]);
 
   return null;
 };
