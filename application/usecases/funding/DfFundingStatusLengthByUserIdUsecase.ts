bd6da1902d602b84f290b84cbabb0d2e1e900de8
m01310g <m01310g@gmail.com>
2025-03-13 20:49:23 +0900

    [remove] 03/13 사용하지 않는 파일 삭제 #395

--- application/usecases/funding/DfFundingStatusLengthByUserIdUsecase.ts
+++
@@ -1,9 +0,0 @@
-import { FundingRepository } from "@/domain/repositories/FundingRepository";
-
-export class DfFundingStatusLengthByUserIdUsecase {
-  constructor(private readonly fundingRepository: FundingRepository) {}
-
-  async execute(userId: string): Promise<number> {
-    return await this.fundingRepository.getFundingStatusByUserId(userId);
-  }
-}
