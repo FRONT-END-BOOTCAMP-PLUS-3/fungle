e78f57da3b53400373ce984407ce7ee12b292816
m01310g <m01310g@gmail.com>
2025-04-11 18:53:39 +0900

    [refactor] 04/11 좋아요 누른 게시글 목록 조회 에러 핸들링 수정 #429

--- app/(main)/user/components/LikedPostList.tsx
+++ app/(main)/user/components/LikedPostList.tsx
@@ -23,17 +23,24 @@ import { useUserLikedPostList } from "./hooks/useUserLikedPostList";
 const LikedPostList = () => {
   const { posts, error, isLoading } = useUserLikedPostList();
 
-  if (isLoading) return <div>로딩중</div>;
+  if (isLoading) {
+    return <div>로딩중</div>;
+  } else if (!posts || posts.length === 0) {
+    return <ErrorMessage>좋아요를 누른 게시글이 없습니다.</ErrorMessage>;
+  }
+
+  if (error) {
+    return (
+      <ErrorMessage>
+        {error instanceof Error
+          ? `에러 발생: ${error.message}`
+          : "좋아요를 누른 게시글을 가져오는 중 알 수 없는 오류가 발생했습니다. 다시 시도해주세요."}
+      </ErrorMessage>
+    );
+  }
 
   return (
     <>
-      {error && (
-        <ErrorMessage>
-          {error instanceof Error
-            ? `에러 발생: ${error.message}`
-            : "좋아요를 누른 게시글을 가져오는 중 알 수 없는 오류가 발생했습니다. 다시 시도해주세요."}
-        </ErrorMessage>
-      )}
       {posts &&
         posts.map((post) => (
           <PostAndLikedListWrapper key={post.id}>
@@ -55,7 +62,7 @@ const LikedPostList = () => {
                         );
                         return field ? field.label : name;
                       })
-                      .join(", ")}{" "}
+                      .join(", ")}
                   </PostRecruitment>
                 )}
                 <PostFooter>
